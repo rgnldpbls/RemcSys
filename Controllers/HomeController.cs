d3c906535ff3ff4c6a8d0f1e8e1c8e74aa21afe5
Regienald Pueblos <101563573+rgnldpbls@users.noreply.github.com>
2024-10-27 21:00:00 +0800

    debug some forms

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -140,8 +140,7 @@ namespace RemcSys.Controllers
                 {
                     evaluator_Name = user.Name,
                     evaluator_Email = user.Email,
-                    field_of_Interest = ["Science",
-                        "Engineering, Architecture, Design, and Built Environment"],
+                    field_of_Interest = ["Engineering, Architecture, Design, and Built Environment", "Science"],
                     UserId = user.Id,
                     UserType = null,
                     center = ["REMC"]
